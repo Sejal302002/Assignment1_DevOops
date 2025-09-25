26c3cc6bac0c1702e5bac4c6af6afb4a538f4d95
sejal <sejalkumbhar30@gmail.com>
2025-09-25 18:49:20 +0530

    Add command-line interface

---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py
+def main():
+    print("Welcome to the Command-Line Calculator!")
+    print("---------------------------------------")
+    # The full functionality will be integrated later.
+
+if __name__ == "__main__":
+    main()
